747ec2070b365efd92346f7f3f47b9cf34b15a1b
Morten Brudvik <morten@brudvik.com>
2018-08-24 07:51:45 +0200

    Initial Commit

---
+++ ambient-player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ambient-player
+//
+//  Created by Morten Brudvik on 24/08/2018.
+//  Copyright © 2018 Morten Brudvik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
