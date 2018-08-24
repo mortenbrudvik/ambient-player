ac93593e9872a00c548927c689586f69731b7137
Morten Brudvik <morten@brudvik.com>
2018-08-24 08:17:19 +0200

    Single View with SnapKit support

---
+++ ambient-player/UIColorExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColorExtension.swift
+//  ambient-player
+//
+//  Created by Morten Brudvik on 24/08/2018.
+//  Copyright © 2018 Morten Brudvik. All rights reserved.
+//
+
+import Foundation
